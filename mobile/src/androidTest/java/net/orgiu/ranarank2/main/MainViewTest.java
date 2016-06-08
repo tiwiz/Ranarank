41d661a41ab3496cff0573f8b4358656a0f70f9f
Roberto Orgiu <roberto.orgiu@gmail.com>
2016-06-08 12:32:52 +0200

    Fixes dependencies

---
+++ mobile/src/androidTest/java/net/orgiu/ranarank2/main/MainViewTest.java
@@ -0,0 +1,14 @@
+package net.orgiu.ranarank2.main;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainViewTest {
+
+    public void loadLayoutFabNotShown() throws Exception {
+
+
+    }
+}
